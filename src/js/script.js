06e4fb8705bbcb8255edee9624e8e764a9f81b4b
Wilian96 <64976355+Wilian96@users.noreply.github.com>
2026-03-08 14:59:48 -0300

    Atualizando Script JS aplicando Operadores Ternarios e Arrow Function

--- src/js/script.js
+++ src/js/script.js
@@ -2,18 +2,32 @@ const horas = document.getElementById('horas');
 const mins = document.getElementById('minutos');
 const segs = document.getElementById('segundos');
 
-const Clock = setInterval(function time() {
+//Código original, sem o operador ternário e Arrow Function (ANTIGO):
+
+// const Clock = setInterval(function time() {
+//     let dateToday = new Date();
+//     let hora = dateToday.getHours();
+//     let min = dateToday.getMinutes();
+//     let seg = dateToday.getSeconds();
+
+//     if (hora < 10) hora = `0${hora}`;
+//     if (min < 10) min = `0${min}`;
+//     if (seg < 10) seg = `0${seg}`;
+
+//     horas.textContent = hora;
+//     mins.textContent = min;
+//     segs.textContent = seg;
+
+// });
+
+const Clock = setInterval(() => {
     let dateToday = new Date();
     let hora = dateToday.getHours();
     let min = dateToday.getMinutes();
     let seg = dateToday.getSeconds();
 
-    if (hora < 10) hora = `0${hora}`;
-    if (min < 10) min = `0${min}`;
-    if (seg < 10) seg = `0${seg}`;
-
-    horas.textContent = hora;
-    mins.textContent = min;
-    segs.textContent = seg;
-
+    horas.textContent = hora < 10 ? `0${hora}` : hora;
+    mins.textContent = min < 10 ? `0${min}` : min;
+    segs.textContent = seg < 10 ? `0${seg}` : seg;
 });
+
